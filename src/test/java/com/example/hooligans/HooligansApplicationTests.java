fad3a0c4f4d215496f85ca1240d7029a33764033
jody816 <81346079+jody816@users.noreply.github.com>
2024-12-16 14:05:56 +0900

    initial settings

---
+++ src/test/java/com/example/hooligans/HooligansApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.hooligans;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class HooligansApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
