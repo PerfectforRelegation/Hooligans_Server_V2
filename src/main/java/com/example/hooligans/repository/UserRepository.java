efeca02d551d3dff5d256ff78ecb3b98b5021e5d
jody816 <81346079+jody816@users.noreply.github.com>
2024-12-18 00:44:48 +0900

    feat: 비동기 방식 Spring Data R2DBC 변경 및 oAuthId 필드로 유저 존재 확인 메서드 추가

--- src/main/java/com/example/hooligans/repository/UserRepository.java
+++ src/main/java/com/example/hooligans/repository/UserRepository.java
@@ -1,8 +1,10 @@
 package com.example.hooligans.repository;
 
 import com.example.hooligans.entity.User;
-import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.repository.reactive.ReactiveCrudRepository;
+import reactor.core.publisher.Mono;
 
-public interface UserRepository extends JpaRepository<User, Long> {
+public interface UserRepository extends ReactiveCrudRepository<User, Long> {
 
+  Mono<Boolean> existsByOAuthId(String oAuthId);
 }
