db2734135abe89447c65b96ca903b477655b6539
jody816 <81346079+jody816@users.noreply.github.com>
2024-12-18 00:42:34 +0900

    feat: 유저 존재 유무 확인 메서드 추가

--- src/main/java/com/example/hooligans/service/UserService.java
+++ src/main/java/com/example/hooligans/service/UserService.java
@@ -1,8 +1,18 @@
 package com.example.hooligans.service;
 
+import com.example.hooligans.repository.UserRepository;
+import lombok.RequiredArgsConstructor;
 import org.springframework.stereotype.Service;
+import reactor.core.publisher.Mono;
 
 @Service
+@RequiredArgsConstructor
 public class UserService {
 
+  private final UserRepository userRepository;
+
+  public Mono<Boolean> isSignUp(String oAuthId) {
+
+    return userRepository.existsByOAuthId(oAuthId);
+  }
 }
