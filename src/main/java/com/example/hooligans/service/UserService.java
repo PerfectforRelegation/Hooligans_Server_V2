3585fc9c189b09f84210f9cabc4801c26e86d115
jody816 <81346079+jody816@users.noreply.github.com>
2024-12-18 00:47:36 +0900

    feat: 유저 정보 반환 메서드 추가

--- src/main/java/com/example/hooligans/service/UserService.java
+++ src/main/java/com/example/hooligans/service/UserService.java
@@ -1,5 +1,7 @@
 package com.example.hooligans.service;
 
+import com.example.hooligans.entity.User;
+import com.example.hooligans.exception.UserNotFoundException;
 import com.example.hooligans.repository.UserRepository;
 import lombok.RequiredArgsConstructor;
 import org.springframework.stereotype.Service;
@@ -14,5 +16,13 @@ public class UserService {
   public Mono<Boolean> isSignUp(String oAuthId) {
 
     return userRepository.existsByOAuthId(oAuthId);
+  }
+
+  public Mono<User> getUserByOAuthId(String oAuthId) {
+
+    return userRepository.findByOAuthId(oAuthId)
+        .switchIfEmpty(
+            Mono.error(new UserNotFoundException("oAuthId 값에 따른 User가 조회되지 않습니다."))
+        );
   }
 }
