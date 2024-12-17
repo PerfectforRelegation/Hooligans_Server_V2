f0b7df156cb650eaee5327f34394d7f0e2d7d201
jody816 <81346079+jody816@users.noreply.github.com>
2024-12-17 17:23:26 +0900

    feat: 카카오 유저 정보 받기 에러 클래스 추가

---
+++ src/main/java/com/example/hooligans/exception/kakao/KakaoGetUserInfoException.java
@@ -0,0 +1,8 @@
+package com.example.hooligans.exception.kakao;
+
+public class KakaoGetUserInfoException extends RuntimeException {
+
+  public KakaoGetUserInfoException(String message) {
+    super(message);
+  }
+}
