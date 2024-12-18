56697536229c532f272e6726254af64ed08f0f1e
jody816 <81346079+jody816@users.noreply.github.com>
2024-12-18 16:54:06 +0900

    fix: 카카오 인증 및 회원 관리 로직 수정

--- src/main/java/com/example/hooligans/controller/KakaoAuthController.java
+++ src/main/java/com/example/hooligans/controller/KakaoAuthController.java
@@ -1,13 +1,10 @@
 package com.example.hooligans.controller;
 
-import com.example.hooligans.dto.kakao.KakaoUserInfo;
-import com.example.hooligans.dto.kakao.KakaoUserResponse;
+import com.example.hooligans.entity.User;
 import com.example.hooligans.exception.kakao.KakaoAuthorizationCodeNullPointerException;
-import com.example.hooligans.exception.kakao.KakaoGetUserInfoException;
 import com.example.hooligans.service.KakaoAuthService;
 import java.util.Map;
 import lombok.RequiredArgsConstructor;
-import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.CrossOrigin;
 import org.springframework.web.bind.annotation.PostMapping;
@@ -25,7 +22,8 @@ public class KakaoAuthController {
   private final KakaoAuthService kakaoAuthService;
 
   @PostMapping("/auth")
-  public Mono<ResponseEntity<KakaoUserResponse>> auth(@RequestBody Map<String, String> payload) {
+  public Mono<ResponseEntity<User>> auth(@RequestBody Map<String, String> payload) {
+
     String code = payload.get("code");
 
     if (code == null || code.isEmpty()) {
@@ -37,27 +35,37 @@ public class KakaoAuthController {
           String accessToken = (String) tokens.get("access_token");
 
           return kakaoAuthService.getUserInfo(accessToken)
-              .handle((userInfo, sink) -> {
-                KakaoUserInfo user = kakaoAuthService.parseUserInfo(userInfo);
-
-                if (user == null || user.getId() == null) {
-                  sink.error(new KakaoGetUserInfoException("사용자 정보 로딩에 실패했습니다."));
-                  return;
-                }
-
-                Long id = user.getId();
-                String nickname = user.getKakaoAccount().getProfile().getNickname();
-                String email = user.getKakaoAccount().getEmail();
-
-                sink.next(new ResponseEntity<>(
-                    KakaoUserResponse.builder()
-                        .id(id)
-                        .nickname(nickname)
-                        .email(email)
-                        .build(),
-                    HttpStatus.OK
-                ));
-              });
+              .flatMap(kakaoUserInfo -> kakaoAuthService.processUserLogin(kakaoUserInfo)
+                  .map(user -> ResponseEntity.ok().body(user))
+              );
         });
+
+//    return kakaoAuthService.getAccessToken(code)
+//        .flatMap(tokens -> {
+//          String accessToken = (String) tokens.get("access_token");
+//
+//          return kakaoAuthService.getUserInfo(accessToken)
+//              .handle((userInfo, sink) -> {
+//                KakaoUserInfo user = kakaoAuthService.parseUserInfo(userInfo);
+//
+//                if (user == null || user.getId() == null) {
+//                  sink.error(new KakaoGetUserInfoException("사용자 정보 로딩에 실패했습니다."));
+//                  return;
+//                }
+//
+//                Long id = user.getId();
+//                String nickname = user.getKakaoAccount().getProfile().getNickname();
+//                String email = user.getKakaoAccount().getEmail();
+//
+//                sink.next(new ResponseEntity<>(
+//                    KakaoUserResponse.builder()
+//                        .id(id)
+//                        .nickname(nickname)
+//                        .email(email)
+//                        .build(),
+//                    HttpStatus.OK
+//                ));
+//              });
+//        });
   }
 }
