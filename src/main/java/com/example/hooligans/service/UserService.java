f19859543b86a75f6dd85a85288146dfb9c06288
jody816 <81346079+jody816@users.noreply.github.com>
2024-12-18 16:51:42 +0900

    feat: 메서드명 수정 및 유저 등록 로직 추가

--- src/main/java/com/example/hooligans/service/UserService.java
+++ src/main/java/com/example/hooligans/service/UserService.java
@@ -1,7 +1,9 @@
 package com.example.hooligans.service;
 
+import com.example.hooligans.dto.kakao.SocialUserResponse;
 import com.example.hooligans.entity.User;
 import com.example.hooligans.exception.UserNotFoundException;
+import com.example.hooligans.exception.UserRegistrationException;
 import com.example.hooligans.repository.UserRepository;
 import lombok.RequiredArgsConstructor;
 import org.springframework.stereotype.Service;
@@ -15,14 +17,22 @@ public class UserService {
 
   public Mono<Boolean> isSignUp(String oAuthId) {
 
-    return userRepository.existsByOAuthId(oAuthId);
+    return userRepository.existsByOauthId(oAuthId);
   }
 
   public Mono<User> getUserByOAuthId(String oAuthId) {
 
-    return userRepository.findByOAuthId(oAuthId)
+    return userRepository.findByOauthId(oAuthId)
         .switchIfEmpty(
             Mono.error(new UserNotFoundException("oAuthId 값에 따른 User가 조회되지 않습니다."))
         );
+  }
+
+  public Mono<User> registerNewUser(SocialUserResponse userResponse) {
+
+    User newUser = userResponse.toEntity();
+
+    return userRepository.save(newUser)
+        .onErrorMap(e -> new UserRegistrationException("유저 등록에 실패했습니다. - " + e.getMessage()));
   }
 }
