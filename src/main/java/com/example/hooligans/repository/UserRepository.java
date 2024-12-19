b76d154e2d3839c8606d88dc78abd2752761a684
jody816 <81346079+jody816@users.noreply.github.com>
2024-12-19 14:29:58 +0900

    feat: 유저 이메일 업데이트 로직 추가

--- src/main/java/com/example/hooligans/repository/UserRepository.java
+++ src/main/java/com/example/hooligans/repository/UserRepository.java
@@ -1,6 +1,8 @@
 package com.example.hooligans.repository;
 
 import com.example.hooligans.entity.User;
+import org.springframework.data.r2dbc.repository.Modifying;
+import org.springframework.data.r2dbc.repository.Query;
 import org.springframework.data.repository.reactive.ReactiveCrudRepository;
 import reactor.core.publisher.Mono;
 
@@ -9,4 +11,11 @@ public interface UserRepository extends ReactiveCrudRepository<User, Long> {
   Mono<Boolean> existsByOauthId(String oAuthId);
 
   Mono<User> findByOauthId(String oAuthId);
+
+  @Modifying
+  @Query("UPDATE users SET email = :newEmail WHERE id = :id")
+  Mono<Integer> updateEmailById(Long id, String newEmail);
+  // 왜 Mono<User>로 하지 않았나?
+  // @Modifying -> insert, update, delete 를 실행할 때 필요
+  // 기본 동작: Mono<Integer> 영향받은 행 수를 반환 -> Mono<User> 반환 지원 X
 }
