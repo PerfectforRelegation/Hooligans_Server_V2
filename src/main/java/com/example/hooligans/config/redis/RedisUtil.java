dba93726daa62f284c831e1196ed4081db555a59
jody816 <81346079+jody816@users.noreply.github.com>
2024-12-23 15:26:11 +0900

    fix: 의존성 수정에 따른 Redis 메서드 수정

--- src/main/java/com/example/hooligans/config/redis/RedisUtil.java
+++ src/main/java/com/example/hooligans/config/redis/RedisUtil.java
@@ -1,30 +1,43 @@
 package com.example.hooligans.config.redis;
 
-import java.util.concurrent.TimeUnit;
+import java.time.Duration;
 import lombok.RequiredArgsConstructor;
-import org.springframework.data.redis.core.RedisTemplate;
+import org.springframework.data.redis.core.ReactiveRedisTemplate;
 import org.springframework.stereotype.Component;
+import reactor.core.publisher.Mono;
 
 @Component
 @RequiredArgsConstructor
 public class RedisUtil {
 
-  private final RedisTemplate<String, Object> redisTemplate;
+  private final ReactiveRedisTemplate<String, Object> reactiveRedisTemplate;
   private static final String LOGIN_PREFIX = "login_";
 
-  public void addRefreshToken(String oauthId, String refreshToken, long expiration) {
-    redisTemplate.opsForValue().set(LOGIN_PREFIX + oauthId, refreshToken, expiration, TimeUnit.MILLISECONDS);
+  public Mono<Boolean> addRefreshToken(String oauthId, String refreshToken, long expirationMillis) {
+    String key = LOGIN_PREFIX + oauthId;
+    return reactiveRedisTemplate
+        .opsForValue()
+        .set(key, refreshToken, Duration.ofMillis(expirationMillis));
   }
 
-  public boolean hasKeyRefreshToken(String oauthId) {
-    return Boolean.TRUE.equals(redisTemplate.hasKey(LOGIN_PREFIX + oauthId));
+  public Mono<Boolean> hasKeyRefreshToken(String oauthId) {
+    String key = LOGIN_PREFIX + oauthId;
+    return reactiveRedisTemplate
+        .hasKey(key);
   }
 
-  public Object getRefreshToken(String oauthId) {
-    return redisTemplate.opsForValue().get(LOGIN_PREFIX + oauthId);
+  public Mono<String> getRefreshToken(String oauthId) {
+    String key = LOGIN_PREFIX + oauthId;
+    return reactiveRedisTemplate
+        .opsForValue()
+        .get(key)
+        .map(Object::toString);
   }
 
-  public void deleteRefreshToken(String oauthId) {
-    redisTemplate.delete(LOGIN_PREFIX + oauthId);
+  public Mono<Boolean> deleteRefreshToken(String oauthId) {
+    String key = LOGIN_PREFIX + oauthId;
+    return reactiveRedisTemplate
+        .delete(key)
+        .map(deletedCount -> deletedCount > 0);
   }
 }
