35b18169db53c9cf1246ee3d68017355a596f9c7
jody816 <81346079+jody816@users.noreply.github.com>
2024-12-23 15:25:55 +0900

    fix: 의존성 수정에 따른 Redis 설정 수정

--- src/main/java/com/example/hooligans/config/redis/RedisConfig.java
+++ src/main/java/com/example/hooligans/config/redis/RedisConfig.java
@@ -3,10 +3,11 @@ package com.example.hooligans.config.redis;
 import org.springframework.beans.factory.annotation.Value;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
-import org.springframework.data.redis.connection.RedisConnectionFactory;
+import org.springframework.data.redis.connection.ReactiveRedisConnectionFactory;
 import org.springframework.data.redis.connection.lettuce.LettuceConnectionFactory;
-import org.springframework.data.redis.core.RedisTemplate;
+import org.springframework.data.redis.core.ReactiveRedisTemplate;
 import org.springframework.data.redis.serializer.GenericJackson2JsonRedisSerializer;
+import org.springframework.data.redis.serializer.RedisSerializationContext;
 import org.springframework.data.redis.serializer.StringRedisSerializer;
 
 @Configuration
@@ -19,18 +20,19 @@ public class RedisConfig {
   private int port;
 
   @Bean
-  public RedisConnectionFactory redisConnectionFactory() {
+  public ReactiveRedisConnectionFactory reactiveRedisConnectionFactory() {
     return new LettuceConnectionFactory(host, port);
   }
 
   @Bean
-  public RedisTemplate<String, Object> redisTemplate() {
+  public ReactiveRedisTemplate<String, Object> reactiveRedisTemplate(ReactiveRedisConnectionFactory connectionFactory) {
 
-    RedisTemplate<String, Object> redisTemplate = new RedisTemplate<>();
-    redisTemplate.setConnectionFactory(redisConnectionFactory());
-    redisTemplate.setKeySerializer(new StringRedisSerializer());
-    redisTemplate.setValueSerializer(new GenericJackson2JsonRedisSerializer());
+    RedisSerializationContext<String, Object> serializationContext =
+        RedisSerializationContext.<String, Object>newSerializationContext()
+            .key(StringRedisSerializer.UTF_8)
+            .value(new GenericJackson2JsonRedisSerializer())
+            .build();
 
-    return redisTemplate;
+    return new ReactiveRedisTemplate<>(connectionFactory, serializationContext);
   }
 }
