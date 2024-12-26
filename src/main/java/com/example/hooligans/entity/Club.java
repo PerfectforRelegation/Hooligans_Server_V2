83b88422cd0a138599c019202d398140db85ea1c
jody816 <81346079+jody816@users.noreply.github.com>
2024-12-26 19:28:00 +0900

    feat: 구단 엔티티 추가

---
+++ src/main/java/com/example/hooligans/entity/Club.java
@@ -0,0 +1,33 @@
+package com.example.hooligans.entity;
+
+import java.math.BigDecimal;
+import java.time.LocalDateTime;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+import org.springframework.data.annotation.Id;
+import org.springframework.data.relational.core.mapping.Column;
+import org.springframework.data.relational.core.mapping.Table;
+
+@Getter
+@NoArgsConstructor
+@Table(name = "clubs")
+public class Club {
+
+  @Id
+  private Long id;
+
+  @Column("name")
+  private String name;
+
+  @Column("symbol")
+  private String symbol;
+
+  @Column("current_price")
+  private BigDecimal currentPrice;
+
+  @Column("total_supply")
+  private BigDecimal totalSupply;
+
+  @Column("created_at")
+  private LocalDateTime createdAt;
+}
