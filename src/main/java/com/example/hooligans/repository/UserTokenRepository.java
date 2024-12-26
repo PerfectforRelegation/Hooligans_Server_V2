cf949f15eaf5d2b8264397d40ef67226b0987f11
jody816 <81346079+jody816@users.noreply.github.com>
2024-12-26 19:27:47 +0900

    remove: 불필요 인터페이스 삭제

--- src/main/java/com/example/hooligans/repository/UserTokenRepository.java
+++
@@ -1,8 +0,0 @@
-package com.example.hooligans.repository;
-
-import com.example.hooligans.entity.UserToken;
-import org.springframework.data.repository.reactive.ReactiveCrudRepository;
-
-public interface UserTokenRepository extends ReactiveCrudRepository<UserToken, Long> {
-
-}
