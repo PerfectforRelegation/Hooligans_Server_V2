28e5a3c6a535d117f8945ab8215ba258b31171fb
jody816 <81346079+jody816@users.noreply.github.com>
2024-12-29 22:15:56 +0900

    feat: 구단 엔티티 레포지토리 추가

---
+++ src/main/java/com/example/hooligans/repository/EplClubRepository.java
@@ -0,0 +1,8 @@
+package com.example.hooligans.repository;
+
+import com.example.hooligans.entity.EplClub;
+import org.springframework.data.repository.reactive.ReactiveCrudRepository;
+
+public interface EplClubRepository extends ReactiveCrudRepository<EplClub, Long> {
+
+}
