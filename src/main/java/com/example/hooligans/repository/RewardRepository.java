81c7733dc2a1236aa96d01c7dd455e4a12d80e10
jody816 <81346079+jody816@users.noreply.github.com>
2024-12-29 22:16:37 +0900

    feat: 보상 엔티티 레포지토리 추가

---
+++ src/main/java/com/example/hooligans/repository/RewardRepository.java
@@ -0,0 +1,8 @@
+package com.example.hooligans.repository;
+
+import com.example.hooligans.entity.Reward;
+import org.springframework.data.repository.reactive.ReactiveCrudRepository;
+
+public interface RewardRepository extends ReactiveCrudRepository<Reward, Long> {
+
+}
