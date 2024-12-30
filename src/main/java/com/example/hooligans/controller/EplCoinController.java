0eef5a4e270b2a1c411a841e4ba037411cf09010
jody816 <81346079+jody816@users.noreply.github.com>
2024-12-30 19:15:02 +0900

    feat: 컨트롤러 추가

---
+++ src/main/java/com/example/hooligans/controller/EplCoinController.java
@@ -0,0 +1,13 @@
+package com.example.hooligans.controller;
+
+import lombok.RequiredArgsConstructor;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+@RequiredArgsConstructor
+@RequestMapping("/epl-coin")
+public class EplCoinController {
+
+
+}
