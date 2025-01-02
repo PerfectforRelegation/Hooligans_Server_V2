2e31ef390830d7daa883fdafc555d3500b253914
jody816 <81346079+jody816@users.noreply.github.com>
2025-01-02 14:19:07 +0900

    feat: 공통 모듈 추가 및 라이브러리화 설정 + 이전 설정 파일 추가

---
+++ MSA-common/src/main/java/com/joh/common/config/redis/RedisConfig.java
@@ -0,0 +1,48 @@
+package com.joh.common.config.redis;
+
+import org.springframework.beans.factory.annotation.Value;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.data.redis.connection.ReactiveRedisConnectionFactory;
+import org.springframework.data.redis.connection.RedisStandaloneConfiguration;
+import org.springframework.data.redis.connection.lettuce.LettuceConnectionFactory;
+import org.springframework.data.redis.core.ReactiveRedisTemplate;
+import org.springframework.data.redis.serializer.GenericJackson2JsonRedisSerializer;
+import org.springframework.data.redis.serializer.RedisSerializationContext;
+import org.springframework.data.redis.serializer.StringRedisSerializer;
+
+@Configuration
+public class RedisConfig {
+
+  @Value("${spring.data.redis.host}")
+  private String host;
+
+  @Value("${spring.data.redis.port}")
+  private int port;
+
+  @Value("${spring.data.redis.password}")
+  private String password;
+
+  @Bean
+  public ReactiveRedisConnectionFactory reactiveRedisConnectionFactory() {
+    RedisStandaloneConfiguration configuration = new RedisStandaloneConfiguration();
+    configuration.setHostName(host);
+    configuration.setPort(port);
+    configuration.setPassword(password);
+    return new LettuceConnectionFactory(configuration);
+  }
+
+  @Bean
+  public ReactiveRedisTemplate<String, Object> reactiveRedisTemplate(ReactiveRedisConnectionFactory connectionFactory) {
+
+    RedisSerializationContext<String, Object> serializationContext = RedisSerializationContext
+        .<String, Object>newSerializationContext(new StringRedisSerializer())
+        .key(StringRedisSerializer.UTF_8) // Key 직렬화
+        .value(new GenericJackson2JsonRedisSerializer()) // Value 직렬화
+        .hashKey(StringRedisSerializer.UTF_8) // Hash Key 직렬화
+        .hashValue(new GenericJackson2JsonRedisSerializer()) // Hash Value 직렬화
+        .build();
+
+    return new ReactiveRedisTemplate<>(connectionFactory, serializationContext);
+  }
+}
