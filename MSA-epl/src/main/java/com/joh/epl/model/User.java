3bd06560b3fd6b4fe5eee33d604443bc4f0d1301
jody816 <81346079+jody816@users.noreply.github.com>
2025-01-05 00:38:44 +0900

    feat: User 컬렉션(테이블) 추가 및 필드 선언

---
+++ MSA-epl/src/main/java/com/joh/epl/model/User.java
@@ -0,0 +1,35 @@
+package com.joh.epl.model;
+
+import java.time.LocalDateTime;
+import lombok.Builder;
+import lombok.Getter;
+import org.springframework.data.annotation.Id;
+import org.springframework.data.mongodb.core.mapping.Document;
+
+@Getter
+@Document(collection = "users") // MongoDB 컬렉션 이름
+public class User {
+
+  @Id
+  private String id; // MongoDB ObjectId와 매핑
+
+  private String oauthProvider;
+  private String oauthId;
+  private String email;
+  private String nickname;
+  private String profileImage;
+  private LocalDateTime registrationDate;
+
+  public User() {}
+
+  @Builder
+  public User(String oauthProvider, String oauthId, String email, String nickname,
+      String profileImage, LocalDateTime registrationDate) {
+    this.oauthProvider = oauthProvider;
+    this.oauthId = oauthId;
+    this.email = email;
+    this.nickname = nickname;
+    this.profileImage = profileImage;
+    this.registrationDate = registrationDate;
+  }
+}
