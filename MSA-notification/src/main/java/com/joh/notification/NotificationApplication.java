6c763f55c4f330e803c7b105512112d07b76489f
jody816 <81346079+jody816@users.noreply.github.com>
2025-01-04 19:01:13 +0900

    feat: 알림 모듈 유레카 설정 추가

--- MSA-notification/src/main/java/com/joh/notification/NotificationApplication.java
+++ MSA-notification/src/main/java/com/joh/notification/NotificationApplication.java
@@ -1,12 +1,16 @@
 package com.joh.notification;
 
+import java.util.TimeZone;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cloud.client.discovery.EnableDiscoveryClient;
 
 @SpringBootApplication
+@EnableDiscoveryClient
 public class NotificationApplication {
 
   public static void main(String[] args) {
+    TimeZone.setDefault(TimeZone.getTimeZone("Asia/Seoul"));
     SpringApplication.run(NotificationApplication.class, args);
   }
 
