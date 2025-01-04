77190a197c29d2261cac666d878172ac4a04b81f
jody816 <81346079+jody816@users.noreply.github.com>
2025-01-04 18:56:25 +0900

    feat: epl 모듈 컴포넌트 스캔 범위 설정

--- MSA-epl/src/main/java/com/joh/epl/EplApplication.java
+++ MSA-epl/src/main/java/com/joh/epl/EplApplication.java
@@ -1,12 +1,16 @@
 package com.joh.epl;
 
+import java.util.TimeZone;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cloud.client.discovery.EnableDiscoveryClient;
 
-@SpringBootApplication
+@SpringBootApplication(scanBasePackages = {"com.joh.common", "com.joh.epl"})
+@EnableDiscoveryClient
 public class EplApplication {
 
   public static void main(String[] args) {
+    TimeZone.setDefault(TimeZone.getTimeZone("Asia/Seoul"));
     SpringApplication.run(EplApplication.class, args);
   }
 
