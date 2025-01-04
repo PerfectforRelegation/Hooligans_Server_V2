f44bdfc4c27b7f7e292097f3e16ab2148ce6c4d3
jody816 <81346079+jody816@users.noreply.github.com>
2025-01-04 18:21:26 +0900

    feat: 컴포넌트 스캔 범위 확장

--- MSA-api-gateway/src/main/java/com/joh/apigateway/ApiGatewayApplication.java
+++ MSA-api-gateway/src/main/java/com/joh/apigateway/ApiGatewayApplication.java
@@ -5,7 +5,7 @@ import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.cloud.client.discovery.EnableDiscoveryClient;
 
-@SpringBootApplication
+@SpringBootApplication(scanBasePackages = {"com.joh.common", "com.joh.apigateway"})
 @EnableDiscoveryClient
 public class ApiGatewayApplication {
 
