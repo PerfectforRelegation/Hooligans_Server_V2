d123464f93bce8cfb26ad24f8a74892a24be225f
jody816 <81346079+jody816@users.noreply.github.com>
2025-01-09 15:27:41 +0900

    feat: core 모듈 notification 컨트롤러 테스트 api 추가

--- MSA-core/src/main/java/com/joh/core/notification/controller/NotificationController.java
+++ MSA-core/src/main/java/com/joh/core/notification/controller/NotificationController.java
@@ -1,12 +1,14 @@
 package com.joh.core.notification.controller;
 
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
 @RestController
+@RequestMapping("/notification")
 public class NotificationController {
 
-  @GetMapping("/notification/test")
+  @GetMapping("/test")
   public String test() {
     return "notification test";
   }
