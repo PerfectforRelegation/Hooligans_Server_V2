814465fc90795958de2475b8560a40ec003ae79b
jody816 <81346079+jody816@users.noreply.github.com>
2025-01-11 02:35:26 +0900

    feat: Coin 모듈 - 스웨거 설정 클래스 추가

---
+++ MSA-coin/src/main/java/com/joh/coin/config/SwaggerConfig.java
@@ -0,0 +1,53 @@
+package com.joh.coin.config;
+
+import io.swagger.v3.oas.annotations.OpenAPIDefinition;
+import io.swagger.v3.oas.models.Components;
+import io.swagger.v3.oas.models.OpenAPI;
+import io.swagger.v3.oas.models.info.Info;
+import io.swagger.v3.oas.models.security.SecurityRequirement;
+import io.swagger.v3.oas.models.security.SecurityScheme;
+import io.swagger.v3.oas.models.security.SecurityScheme.In;
+import io.swagger.v3.oas.models.security.SecurityScheme.Type;
+import io.swagger.v3.oas.models.servers.Server;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+
+@OpenAPIDefinition
+@Configuration
+public class SwaggerConfig {
+
+    @Bean
+    public OpenAPI openAPI() {
+
+        SecurityScheme securityScheme = new SecurityScheme()
+            .name("Authorization")
+            .type(Type.HTTP)
+            .scheme("bearer")
+            .bearerFormat("JWT")
+            .in(In.HEADER);
+
+        SecurityScheme userIdScheme = new SecurityScheme()
+            .name("X-USER-ID")
+            .type(Type.APIKEY) // API Key 타입
+            .in(In.HEADER);    // 헤더로 전달
+
+        return new OpenAPI()
+            .components(new Components()
+                .addSecuritySchemes("Authorization", securityScheme)
+                .addSecuritySchemes("X-USER-ID", userIdScheme)
+            )
+            .addSecurityItem(new SecurityRequirement()
+                .addList("Authorization") // 보안 적용
+                .addList("X-USER-ID")
+            )
+            .addServersItem(new Server().url("/coin"))
+            .info(apiInfo());
+    }
+
+    private Info apiInfo() {
+        return new Info()
+            .title("API Test")          // API 제목
+            .description("API 테스트 swagger")  // API에 대한 설명
+            .version("1.0.0");
+    }
+}
