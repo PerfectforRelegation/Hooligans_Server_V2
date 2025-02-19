002f8ef59560fb4260c5aad9c9a82bee3961c3a0
jody816 <81346079+jody816@users.noreply.github.com>
2025-02-19 20:04:49 +0900

    feat: coin 모듈 - 주문 테이블 컬럼 수정 및 상태 변환 메서드 추가

--- MSA-coin/src/main/java/com/joh/coin/entity/OrderBook.java
+++ MSA-coin/src/main/java/com/joh/coin/entity/OrderBook.java
@@ -2,6 +2,7 @@ package com.joh.coin.entity;
 
 import java.math.BigDecimal;
 import java.time.LocalDateTime;
+import lombok.Builder;
 import lombok.Getter;
 import lombok.NoArgsConstructor;
 import org.springframework.data.annotation.Id;
@@ -23,13 +24,10 @@ public class OrderBook {
   private Long eplCoinId;
 
   @Column("order_type")
-  private String orderType; // ENUM ('LIMIT', 'MARKET')
-
-  @Column("coin_amount")
-  private BigDecimal coinAmount;
+  private String orderType; // ENUM 매수냐 매도냐
 
   @Column("order_price")
-  private BigDecimal orderPrice;
+  private Long orderPrice;
 
   @Column("status")
   private String status; // 'PENDING', 'COMPLETED', 'CANCELED'
@@ -39,4 +37,19 @@ public class OrderBook {
 
   @Column("updated_at")
   private LocalDateTime updatedAt;
+
+  public void changeStatusToCompleted() {
+    this.status = "COMPLETED";
+  }
+
+  @Builder
+  public OrderBook(String userId, Long eplCoinId, String orderType, Long orderPrice, String status,
+      LocalDateTime createdAt) {
+    this.userId = userId;
+    this.eplCoinId = eplCoinId;
+    this.orderType = orderType;
+    this.orderPrice = orderPrice;
+    this.status = status;
+    this.createdAt = createdAt;
+  }
 }
