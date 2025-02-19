6e0d65a9747425d647d24751c61dbb0a94b0f569
jody816 <81346079+jody816@users.noreply.github.com>
2025-02-19 20:05:42 +0900

    feat: coin 모듈 - 매도 데이터 가져 오는 메서드 추가
    메서드 수정 필요

--- MSA-coin/src/main/java/com/joh/coin/repository/OrderBookRepository.java
+++ MSA-coin/src/main/java/com/joh/coin/repository/OrderBookRepository.java
@@ -1,8 +1,13 @@
 package com.joh.coin.repository;
 
 import com.joh.coin.entity.OrderBook;
+import java.math.BigDecimal;
+import org.springframework.data.r2dbc.repository.Query;
 import org.springframework.data.repository.reactive.ReactiveCrudRepository;
+import reactor.core.publisher.Flux;
+import reactor.core.publisher.Mono;
 
 public interface OrderBookRepository extends ReactiveCrudRepository<OrderBook, Long> {
 
+  Flux<OrderBook> findByEplCoinIdAndOrderPriceOrderByCreatedAt(Long eplCoinId, Long orderPrice);
 }
