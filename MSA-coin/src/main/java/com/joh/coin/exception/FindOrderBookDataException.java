e045dc5f9c7ca706570f8f7bb56385fc1676a259
jody816 <81346079+jody816@users.noreply.github.com>
2025-02-26 02:21:50 +0900

    feat: coin 모듈 - 주문 데이터 조회 에러 클래스 추가

---
+++ MSA-coin/src/main/java/com/joh/coin/exception/FindOrderBookDataException.java
@@ -0,0 +1,8 @@
+package com.joh.coin.exception;
+
+public class FindOrderBookDataException extends RuntimeException {
+
+  public FindOrderBookDataException(String message) {
+    super(message);
+  }
+}
