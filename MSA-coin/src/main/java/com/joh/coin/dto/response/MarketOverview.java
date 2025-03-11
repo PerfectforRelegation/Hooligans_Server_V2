67ba3b28684c31dfd59b7a673926c119b994bd77
jody816 <81346079+jody816@users.noreply.github.com>
2025-03-11 11:04:05 +0900

    feat: Coin 모듈 - 기본 시장 데이터 DTO 추가

---
+++ MSA-coin/src/main/java/com/joh/coin/dto/response/MarketOverview.java
@@ -0,0 +1,12 @@
+package com.joh.coin.dto.response;
+
+import lombok.Builder;
+import lombok.Getter;
+
+@Getter
+@Builder
+public class MarketOverview {
+
+  private UserAsset userAsset;
+  private CoinDTO
+}
