7e577b3525bba4c3befea7c40c9ef897f822bc20
jody816 <81346079+jody816@users.noreply.github.com>
2025-03-11 14:23:45 +0900

    feat: common 모듈 - 데이터 저장 및 리스트 조회 메서드 추가

--- MSA-common/src/main/java/com/joh/common/redis/RedisUtil.java
+++ MSA-common/src/main/java/com/joh/common/redis/RedisUtil.java
@@ -1,5 +1,26 @@
 package com.joh.common.redis;
 
+import lombok.RequiredArgsConstructor;
+import org.springframework.data.redis.core.ReactiveRedisTemplate;
+import org.springframework.stereotype.Component;
+import reactor.core.publisher.Flux;
+import reactor.core.publisher.Mono;
+
+@Component
+@RequiredArgsConstructor
 public class RedisUtil {
 
+  private final ReactiveRedisTemplate<String, Object> reactiveRedisTemplate;
+
+  public <T> Mono<Void> saveObject(String key, T data) {
+    return reactiveRedisTemplate.opsForList()
+        .rightPush(key, data)
+        .then();
+  }
+
+  public <T> Flux<T> getObjectList(String key, Class<T> clazz) {
+    return reactiveRedisTemplate.opsForList()
+        .range(key, 0, -1) // 전체 리스트 조회
+        .cast(clazz);                // 타입 변환
+  }
 }
