e6347ebd7c1043611ae787b2dccdf36c351bd406
jody816 <81346079+jody816@users.noreply.github.com>
2025-03-13 14:29:19 +0900

    refactor: CoinRepository 공통 모듈로 이동

--- MSA-common/src/main/java/com/joh/common/coin/CoinRepository.java
+++ MSA-common/src/main/java/com/joh/common/coin/CoinRepository.java
@@ -1,6 +1,6 @@
-package com.joh.coin.repository;
+package com.joh.common.coin;
 
-import com.joh.coin.entity.Coin;
+import com.joh.common.coin.Coin;
 import org.springframework.data.repository.reactive.ReactiveCrudRepository;
 
 public interface CoinRepository extends ReactiveCrudRepository<Coin, Long> {
