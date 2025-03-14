c38ef07a735ded835c79fbe9d3507977629b82db
jody816 <81346079+jody816@users.noreply.github.com>
2025-03-14 14:32:33 +0900

    feat: Overview 모듈 - 유레카 관련 어노테이션, 레포지토리 패키지 스캔 추가

--- MSA-coin-overview/src/main/java/com/joh/overview/CoinOverviewApplication.java
+++ MSA-coin-overview/src/main/java/com/joh/overview/CoinOverviewApplication.java
@@ -3,8 +3,12 @@ package com.joh.overview;
 import java.util.TimeZone;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cloud.client.discovery.EnableDiscoveryClient;
+import org.springframework.data.r2dbc.repository.config.EnableR2dbcRepositories;
 
 @SpringBootApplication(scanBasePackages = {"com.joh.common", "com.joh.overview"})
+@EnableR2dbcRepositories(basePackages = {"com.joh.common.coin", "com.joh.overview.repository"})
+@EnableDiscoveryClient
 public class CoinOverviewApplication {
 
   public static void main(String[] args) {
