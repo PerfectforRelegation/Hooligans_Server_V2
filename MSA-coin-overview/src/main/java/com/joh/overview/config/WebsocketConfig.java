e376683339701a7e0caeb4f138ca922d7f86b6f5
jody816 <81346079+jody816@users.noreply.github.com>
2025-03-25 19:19:56 +0900

    feat: Overview 모듈 - 웹소켓 Config 핸들러 엔드포인트 설정 추가

--- MSA-coin-overview/src/main/java/com/joh/overview/config/WebsocketConfig.java
+++ MSA-coin-overview/src/main/java/com/joh/overview/config/WebsocketConfig.java
@@ -1,5 +1,6 @@
 package com.joh.overview.config;
 
+import com.joh.overview.handler.CandleStickWebSocketHandler;
 import com.joh.overview.handler.CoinWebSocketHandler;
 import java.util.Map;
 import org.springframework.context.annotation.Bean;
@@ -11,9 +12,13 @@ import org.springframework.web.reactive.handler.SimpleUrlHandlerMapping;
 public class WebsocketConfig {
 
   @Bean
-  public HandlerMapping webSocketMapping(CoinWebSocketHandler coinWebSocketHandler) {
+  public HandlerMapping webSocketMapping(
+      CoinWebSocketHandler coinWebSocketHandler,
+      CandleStickWebSocketHandler candleStickWebSocketHandler
+  ) {
     return new SimpleUrlHandlerMapping(Map.of(
-        "/coins", coinWebSocketHandler // ws://localhost:8080/coin/coins
+        "/coins", coinWebSocketHandler, // ws://localhost:8080/coin/coins -> 모든 코인 실시간 데이터
+        "/coins/{symbol}/candlestick", candleStickWebSocketHandler // 특정
     ), 1);
   }
 }
