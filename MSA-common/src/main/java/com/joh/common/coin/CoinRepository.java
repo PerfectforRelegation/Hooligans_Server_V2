90566be499e8d430de7abae74b6cfeaa2f6a128a
jody816 <81346079+jody816@users.noreply.github.com>
2025-03-25 19:08:05 +0900

    feat: Common 모듈 - Coin 조회 메서드 추가

--- MSA-common/src/main/java/com/joh/common/coin/CoinRepository.java
+++ MSA-common/src/main/java/com/joh/common/coin/CoinRepository.java
@@ -1,7 +1,12 @@
 package com.joh.common.coin;
 
+import com.joh.common.coin.projection.IdAndNameAndSymbolProjection;
+import org.springframework.data.r2dbc.repository.Query;
 import org.springframework.data.repository.reactive.ReactiveCrudRepository;
+import reactor.core.publisher.Flux;
 
 public interface CoinRepository extends ReactiveCrudRepository<Coin, Long> {
 
+  @Query("SELECT id, name, symbol FROM coins")
+  Flux<IdAndNameAndSymbolProjection> findCoinsSummaryProjection();
 }
